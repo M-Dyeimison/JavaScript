57ec495d0d0bfe66b4e43bbe3059ae4bb4b13407
M-Dyeimison <marcosdyeimison@gmail.com>
2022-08-25 19:40:10 -0300

    update

---
+++ JavaScript/Selecionando elementos/selecting-elements.js
@@ -0,0 +1,9 @@
+function show() {
+    // let elements = document.querySelectorAll("div#phones input[name='phones']")
+    // let element = document.getElementById("name")
+    // console.log(element.value)
+    // let elements = document.getElementsByName("phone")
+    // console.log(elements[0].value)
+    let elements = document.getElementsByTagName("input")
+    console.log(elements)
+}
--- JavaScript/exercicio classes 2/spaceship.js
+++ JavaScript/exercicio classes 2/spaceship.js
@@ -16,47 +16,49 @@ class Spaceship {
     }
 }
 
-function spaceshipType (name, crewQuantity) {
-    let spaceship_Type
-
-    while(![1, 2].includes(spaceship_type)){
-       spaceship_Type = Number(prompt("Tipo de nave: \n1- Nave de batalha\n2- Nave de transporte"))
+class App {
+    constructor() {
+        this.spaceship = null
     }
 
-    if(spaceship_Type == 1) {
-        weaponQuantity = Number(prompt("Número de armas: "))
-        let spaceship = new BattleSpaceship(name, crewQuantity, weaponQuantity)
-        spaceship.type = spaceship_Type
-        return spaceship
-    }else if(spaceship_Type == 2) {
-        seatsQuantity = Number(prompt("Número de lugares: "))
-        let spaceship = new TransportSpaceship(name, crewQuantity, seatsQuantity)
-        spaceship.type = spaceship_Type
-        return spaceship
+    spaceshipType (name, crewQuantity) {
+        let spaceship_Type
+    
+        while(![1, 2].includes(spaceship_type)){
+           spaceship_Type = Number(prompt("Tipo de nave: \n1- Nave de batalha\n2- Nave de transporte"))
+        }
+    
+        if(spaceship_Type == 1) {
+            weaponQuantity = Number(prompt("Número de armas: "))
+            this.spaceship = new BattleSpaceship(name, crewQuantity, weaponQuantity)
+            this.spaceship.type = spaceship_Type
+            return spaceship
+        }else if(spaceship_Type == 2) {
+            seatsQuantity = Number(prompt("Número de lugares: "))
+            this.spaceship = new TransportSpaceship(name, crewQuantity, seatsQuantity)
+            this.spaceship.type = spaceship_Type
+            return spaceship
+        }
+    }
+    
+    createSpaceship() {
+        let name = prompt("Nome da nave espacial: ")
+        let crewQuantity = Number(prompt("Número de passageiros: "))
+        this.spaceship = spaceshipType(name, crewQuantity)
+    }
+
+    menu(spaceship) {
+        let answer = Number(prompt("1- Acelerar a nave\n2- Trocar a nave\n3- Imprimir e sair"))
+        if(answer == 1) {
+            let acceleration = Number(prompt("Aceleração: "))
+            spaceship.speedUp(acceleration)
+            menu(spaceship)
+        }else if(answer == 2) {
+            spaceship = createSpaceship()
+            menu(spaceship)
+        }else if(answer == 3) {
+            if(spaceship.type == 1) alert("Nome: " + spaceship.name + "\nQuantidade de tripulantes: " + spaceship.crewQuantity + "\nVelocidade atual: " + spaceship.currentSpeed + "\nNúmero de armas: " + spaceship.weaponQuantity)
+            else alert("Nome: " + spaceship.name + "\nQuantidade de tripulantes: " + spaceship.crewQuantity + "\nVelocidade atual: " + spaceship.currentSpeed + "\nNúmero de lugares: " + spaceship.seatsQuantity)
+        }
     }
 }
-
-function createSpaceship() {
-    let name = prompt("Nome da nave espacial: ")
-    let crewQuantity = Number(prompt("Número de passageiros: "))
-    let spaceship = spaceshipType(name, crewQuantity)
-    return spaceship
-}
-
-function menu(spaceship) {
-    let answer = Number(prompt("1- Acelerar a nave\n2- Trocar a nave\n3- Imprimir e sair"))
-    if(answer == 1) {
-        let acceleration = Number(prompt("Aceleração: "))
-        spaceship.speedUp(acceleration)
-        menu(spaceship)
-    }else if(answer == 2) {
-        spaceship = createSpaceship()
-        menu(spaceship)
-    }else if(answer == 3) {
-        if(spaceship.type == 1) alert("Nome: " + spaceship.name + "\nQuantidade de tripulantes: " + spaceship.crewQuantity + "\nVelocidade atual: " + spaceship.currentSpeed + "\nNúmero de armas: " + spaceship.weaponQuantity)
-        else alert("Nome: " + spaceship.name + "\nQuantidade de tripulantes: " + spaceship.crewQuantity + "\nVelocidade atual: " + spaceship.currentSpeed + "\nNúmero de lugares: " + spaceship.seatsQuantity)
-    }
-}
-
-let spaceship = createSpaceship()
-menu(spaceship)
